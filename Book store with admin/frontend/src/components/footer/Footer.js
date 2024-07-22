e7a9dd732a4bfc08b89d5eb728e35c9580c88670
Manyagupta0612 <105008331+Manyagupta0612@users.noreply.github.com>
2024-07-22 14:30:53 +0530

    Update Footer.js

--- Book store with admin/frontend/src/components/footer/Footer.js
+++ Book store with admin/frontend/src/components/footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className='bg-zinc-800 text-white px-8  py-4'>
       <h1 className='text-2xl font-semibold text-center'>
-        &copy; 2024, Made With Love By THE CODE MASTER
+        &copy; 2024, Made With Love By MANYA GUPTA
       </h1>
     </div>
   )
